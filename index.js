1172c914d58f52eff35eea51236130136d1b39e3
formago <formago.max.dev@gmail.com>
2017-10-24 01:43:09 +0300

    3 commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+var port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
 });
 
-app.listen(5000);
+app.listen(port);
